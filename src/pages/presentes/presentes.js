a009266cb075a4a0262f9afbfdf72459be4472fc
Vitor Takara <vitor@streamshop.com.br>
2023-06-09 20:02:17 -0300

    feat: pagamento confirmado

--- src/pages/presentes/presentes.js
+++ src/pages/presentes/presentes.js
@@ -2,34 +2,57 @@ import '/public/styles/main.scss';
 import './presentes.scss';
 import '/public/styles/components/vc-button.scss';
 import '/public/styles/components/vc-input.scss';
-import { vcFooterHtml } from '/public/components/vc-footer/vc-footer.js';
-import { vcHeaderHtml } from '/public/components/vc-header/vc-header.js';
+import {
+    vcFooterHtml
+} from '/public/components/vc-footer/vc-footer.js';
+import {
+    vcHeaderHtml
+} from '/public/components/vc-header/vc-header.js';
 document.querySelector('#vc-footer').innerHTML = vcFooterHtml;
 document.querySelector('#vc-header').innerHTML = vcHeaderHtml;
 
 let step = 1;
+
 const presentList = [{
-    title: 'Casa',
-    price: 2000,
-    img: 'https://placekitten.com/g/300/300'
-},{
-    title: 'Ap',
+    id: 1,
+    title: 'Bronze 🤎',
+    price: 250,
+    img: '../../assets/images/gifts/250.jpg'
+}, {
+    id: 2,
+    title: 'Prata 🤍',
+    price: 500,
+    img: '../../assets/images/gifts/500.jpg'
+}, {
+    id: 3,
+    title: 'Ouro 💛',
+    price: 1000,
+    img: '../../assets/images/gifts/1000.jpg'
+}, {
+    id: 4,
+    title: 'Master 💷',
+    price: 2500,
+    img: '../../assets/images/gifts/2500.jpg'
+}, {
+    id: 5,
+    title: 'Diamante 💎',
     price: 5000,
-    img: 'https://placekitten.com/g/300/300'
-},{
-    title: 'Mansao',
-    price: 150000,
-    img: 'https://placekitten.com/g/300/300'
+    img: '../../assets/images/gifts/5000.jpg'
+}, {
+    id: 6,
+    title: '✨ Super ✨',
+    price: 10000,
+    img: '../../assets/images/gifts/10000.jpg'
 }]
 
 window.buildPresentList = () => {
-    const gridPresentRef = document.querySelector("#step-1");
+    const gridPresentRef = document.querySelector("#step-1-presents-container");
     presentList.forEach(present => gridPresentRef.insertAdjacentHTML("beforeend", getPresentCardTemplate(present)));
 }
 
 window.getPresentCardTemplate = (present) => {
     return `
-        <div class="present-card">
+        <div class="present-card" onclick="selectGift(${present.id})">
             <div class="image">
                 <img src="${present.img}">
             </div>
@@ -38,7 +61,7 @@ window.getPresentCardTemplate = (present) => {
             <span class="price">${present.price.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}</span>
 
             <div class="btn-wrapper">
-                <button onclick="changeStep(2)" class="vc-button -small -fill">COMPRAR</button>
+                <button class="vc-button -small --thin">PRESENTEAR</button>
             </div>
         </div>
     `;
@@ -49,6 +72,37 @@ window.changeStep = (stepNumber) => {
     updateView();
 }
 
+window.goToPayment = () => {
+    let name = document.querySelector("#name").value;
+    let message = document.querySelector("#message").value;
+
+    if (!name) {
+        alert("Por favor preencha nome e sobrenome")
+        return;
+    }
+
+    changeStep(3);
+}
+
+window.openMercadoPago = () => {
+  
+}
+
+window.selectGift = (presentId) => {
+    const giftSelected = presentList.find(present => present.id === presentId);
+
+    let giftSelectedImg = document.querySelector("#gift-selected-img");
+    giftSelectedImg.style.background = `url(${giftSelected.img})`;
+    giftSelectedImg.style.backgroundPosition = 'center';
+    giftSelectedImg.style.backgroundSize = 'cover';
+
+    document.querySelector("#gift-selected-name").innerHTML = giftSelected.title;
+
+    localStorage.setItem("gift", giftSelected.price);
+
+    changeStep(2);
+}
+
 window.updateView = () => {
     // Some com todo mundo
     document.querySelectorAll(".step").forEach(step => step.classList.add('-hide'));
@@ -57,31 +111,11 @@ window.updateView = () => {
     document.querySelector(`#step-${step}`).classList.remove('-hide');
 
     // Tela de sucesso handler
-    if(step === 4)
+    if (step === 6)
         setTimeout(() => {
             step = 1;
-            updateView();  
+            updateView();
         }, 5000);
 }
 
-window.generatePixQrCode = () => {
-    // const pixKey = "+5511966378394";
-    // const value = "100.00";
-
-    
-    // const qrCodePix = QrCodePix({
-    //     version: '01',
-    //     key: pixKey, //or any PIX key
-    //     name: 'Vitor Teste',
-    //     city: 'SAO PAULO',
-    //     transactionId: '00020101021126360014', //max 25 characters
-    //     message: 'Pay me :)',
-    //     cep: '06719500',
-    //     value: 150.99,
-    // });
-
-    new QRCode(document.getElementById("qrcode"), 'blabla');
-}
-
 buildPresentList();
-generatePixQrCode();
