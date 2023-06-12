acfb17caebfd995b09cf525be4927b62fa56d264
Vitor Takara <vitor@streamshop.com.br>
2023-06-12 20:21:46 -0300

    b

--- components/vc-header/vc-header.js
+++ components/vc-header/vc-header.js
@@ -6,12 +6,13 @@ import logo from '../../../assets/images/logos/logo_2.svg';
 export const vcHeaderHtml = `
     <header class="vc-header">
         <div class="wrapper">
-            <a class="link" href="https://vitortakara.github.io/vc-casaments/pages/home/>Inicio</a>
-            <div class="logo">
-                <img src="${logo}">
-            </div>
+            <a class="link" href="https://vitortakara.github.io/vc-casaments/pages/home/">Inicio</a>
             
-            <a class="link" href="https://vitortakara.github.io/vc-casaments/pages/presentes/>Presentes</a>
+            <a class="logo" href="https://vitortakara.github.io/vc-casaments/pages/home/">
+                <img src="${logo}">
+            </a>
+            
+            <a class="link" href="https://vitortakara.github.io/vc-casaments/pages/presentes/">Presentes</a>
         </div>
     </header>
 `
